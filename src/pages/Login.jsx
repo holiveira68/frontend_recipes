8c148d20782528f0d4d37c542cdf8ef1590301f1
holiveira68 <h.manuel@aluno.ifsp.edu.br>
2026-05-12 15:49:09 -0300

    inclui div para colocar os 3 elementos em coluna

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,14 +8,17 @@ export default function Login() {
         <>
             <form method="POST">
                 <div className="flex items-center justify-center flex-col gap-1">
-                    <label htmlFor="email">Email</label>
-                    <input className="bg-gray-100 rounded border-emerald-200" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+                    <div >
+                        <label htmlFor="email">Email</label>
+                        <input className="bg-gray-100 rounded border-emerald-200" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input className="bg-gray-100 rounded border-emerald-200" type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <button className="flex items-center justify-center flex-col gap-1 bg-lime-800 text-white font-bold rounded px-4 py-1" type="submit" >Login</button>
                 </div>
-                <div className="flex items-center justify-center flex-col gap-1">
-                    <label htmlFor="password">Password</label>
-                    <input className="bg-gray-100 rounded border-emerald-200" type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                <button className="flex items-center justify-center flex-col gap-1 bg-lime-800 text-white font-bold rounded px-4 py-1" type="submit" >Login</button>
             </form>
         </>
     );
