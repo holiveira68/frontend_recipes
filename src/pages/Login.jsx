8d98125fa52482a49a7aeac1281807fcf22345e4
holiveira68 <h.manuel@aluno.ifsp.edu.br>
2026-05-12 15:33:08 -0300

    ..

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,15 +7,15 @@ export default function Login() {
     return (
         <>
             <form method="POST">
-                <div className="flex flex-col gap-1">
+                <div className="flex items-center justify-center flex-col gap-1">
                     <label htmlFor="email">Email</label>
                     <input className="rounded" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 </div>
-                <div className="flex flex-col gap-1">
+                <div className="flex items-center justify-center flex-col gap-1">
                     <label htmlFor="password">Password</label>
                     <input className="rounded" type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
-                <button className="bg-lime-800 text-white font-bold rounded px-4 py-1" type="submit">Login</button>
+                <button className="items-center justify-center bg-lime-800 text-white font-bold rounded px-4 py-1" type="submit">Login</button>
             </form>
         </>
     );
