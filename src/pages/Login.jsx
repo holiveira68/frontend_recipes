bed111e75899a77de9dbb107d8f573e8092c4bb8
holiveira68 <h.manuel@aluno.ifsp.edu.br>
2026-05-12 15:20:40 -0300

    Fix login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
 
 export default function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const navigate = useNavigate()
 
     return (
         <>
