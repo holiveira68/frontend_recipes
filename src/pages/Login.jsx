db31067353fa5acb777c87399e7437d952d8fb88
holiveira68 <h.manuel@aluno.ifsp.edu.br>
2026-05-12 16:01:56 -0300

    margem alterada

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,12 +10,12 @@ export default function Login() {
                 <div className="flex items-center justify-center flex-col gap-3">
 
                     <div >
-                        <label htmlFor="email">   Email : </label>
-                        <input className="bg-gray-100 rounded border-emerald-200" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <label className="ml-4 mt-4" htmlFor="email">   Email : </label>
+                        <input className="bg-gray-100 rounded border-green-900 border-2" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div>
-                        <label htmlFor="password">Password :</label>
-                        <input className="bg-gray-100 rounded border-emerald-200 border-2" type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <label className="ml-4" htmlFor="password">Password :</label>
+                        <input className="bg-gray-100 rounded border-green-900 border-2" type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <button className="gap-3 mt-5 bg-lime-800 text-white font-bold rounded px-4 py-1" type="submit" >Login</button>
                 </div>
